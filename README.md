d2daa8aa914c856bca697cffd00e19de4d928bdd
Florine W. Dekker <florine@fwdekker.com>
2022-05-05 13:46:01 +0200

    Fix incorrect facial recognition errors

--- README.md
+++ README.md
@@ -6,13 +6,15 @@ Given a sequence of input photos, rotates and centres the photos, and outputs th
 ## Requirements
 - Python 3.9
 - Install requirements from `requirements.txt`
+  - `dlib` requires `cmake` (e.g. `apt install cmake`)
+- [shape_predictor_68_face_landmarks.dat](http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2)
 - [FFmpeg](https://ffmpeg.org/) (e.g. `apt install ffmpeg`)
 
 ## How to use
 1. Put images in `input/` folder.
 2. Rename photos to sequential numbering with
     ```bash
-    $> ls -v | cat -n | while read n f; do mv -n "$f" "$n.jpg"; done
+    ls -v | cat -n | while read n f; do mv -n "$f" "$n.jpg"; done
     ```
 3. Run this script.
 4. Run ffmpeg on the `output/` folder, for example:
--- main.py
+++ main.py
@@ -1,89 +1,116 @@
 import glob
 import math
 import os
+import shutil
 from pathlib import Path
 
 import cv2
+import dlib
 import imutils
 import numpy as np
+from tqdm import tqdm
 
 
-eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
+enable_debug = False
 
 input_dir = "input/"
 output_dir = "output/"
+output_temp_dir = "output/temp/"
+output_final_dir = "output/final/"
+
+shutil.rmtree(output_dir)
 Path(output_dir).mkdir(exist_ok=True)
+Path(output_temp_dir).mkdir(exist_ok=True)
+Path(output_final_dir).mkdir(exist_ok=True)
 
 input_files = os.listdir(input_dir)
+shape_predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-# Pre-process images
-print("# Pre-processing")
-all_eyes = []
-for input_file in glob.glob(f"{input_dir}/*.jpg"):
-    print(input_file)
-    image = cv2.imread(input_file)
+# Pre-process
+eyes_left = {}
+eyes_right = {}
+for input_file in tqdm(glob.glob(f"{input_dir}/*.jpg"), desc="Pre-processing"):
+    image = cv2.cvtColor(cv2.imread(input_file), cv2.COLOR_BGR2RGB)
+    detector = dlib.get_frontal_face_detector()
+    detections = detector(image, 1)
 
-    eyes = eye_cascade.detectMultiScale(image, 1.1, 4).tolist()
-    eyes.sort(key=lambda it: it[2])  # Sort by size, last ones are largest
-    eyes = eyes[-2:]
-    eyes.sort(key=lambda it: it[0])  # Sort by x, so left eye is first
-    all_eyes.append(eyes)
-all_eyes = np.array(all_eyes)
+    faces = dlib.full_object_detections()
+    for detection in detections:
+        faces.append(shape_predictor(image, detection))
 
-avg_center = np.mean(np.mean(all_eyes[:, :, 0:2], axis=0), axis=0)
-avg_distance = np.mean([math.sqrt(it[0] ** 2 + it[1] ** 2) for it in all_eyes[:, 1, 0:2] - avg_center])
+    if len(faces) > 1:
+        raise Exception(f"Too many faces: Found {len(faces)} in '{input_file}'.")
+    face = faces[0]
+
+    eyes_left[input_file] = np.mean(np.array([(face.part(i).x, face.part(i).y) for i in range(42, 48)]), axis=0)
+    eyes_right[input_file] = np.mean(np.array([(face.part(i).x, face.part(i).y) for i in range(36, 42)]), axis=0)
+
+# Calculate statistics
+eyes_dist = {it: math.dist(eyes_left[it], eyes_right[it]) for it in eyes_left.keys()}
+eyes_dist_avg = np.mean(np.array(list(eyes_dist.values())))
+
+eyes_left_np = np.array(list(eyes_left.values()))
+eyes_right_np = np.array(list(eyes_right.values()))
+
+eyes_left_avg = np.mean(eyes_left_np, axis=0)
+eyes_right_avg = np.mean(eyes_right_np, axis=0)
+eyes_center_avg = np.mean([eyes_left_avg, eyes_right_avg], axis=0)
 
 # Translate, rotate, resize
-print("# Translating, rotating, resizing")
-max_width, max_height = [0, 0]
-for input_file in glob.glob(f"{input_dir}/*.jpg"):
-    print(input_file)
-
+width_min, height_min = [1e99, 1e99]
+for input_file in tqdm(glob.glob(f"{input_dir}/*.jpg"), desc="Translating, rotating, resizing"):
     image = cv2.imread(input_file)
+
     height, width = image.shape[:2]
+    eye_left, eye_right = eyes_left[input_file], eyes_right[input_file]
+    eye_center = np.mean([eye_left, eye_right], axis=0)
 
-    eyes = eye_cascade.detectMultiScale(image, 1.1, 4).tolist()
-    eyes.sort(key=lambda it: it[2])  # Sort by size, last ones are largest
-    eyes = eyes[-2:]
-    eyes.sort(key=lambda it: it[0])  # Sort by x, so left eye is first
+    # Draw debug information
+    if enable_debug:
+        image = cv2.circle(image, eye_left.astype(int), radius=20, color=(0, 255, 0), thickness=-1)
+        image = cv2.circle(image, eye_right.astype(int), radius=20, color=(0, 255, 0), thickness=-1)
+        image = cv2.circle(image, eyes_left_avg.astype(int), radius=20, color=(0, 0, 255), thickness=-1)
+        image = cv2.circle(image, eyes_right_avg.astype(int), radius=20, color=(0, 0, 255), thickness=-1)
+        image = cv2.circle(image, eye_center.astype(int), radius=20, color=(255, 0, 0), thickness=-1)
 
-    # for (x, y, w, h) in eyes:
-    #     cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-    eyes_center = np.mean(np.array(eyes)[:, 0:2], axis=0)
-    translation = [avg_center[0] - eyes_center[0], avg_center[1] - eyes_center[1]]
+    # Translate
+    eyes_center = np.mean([eye_left, eye_right], axis=0)
+    translation = [eyes_center_avg[0] - eyes_center[0], eyes_center_avg[1] - eyes_center[1]]
     T = np.float32([[1, 0, translation[0]], [0, 1, translation[1]]])
     image = cv2.warpAffine(image, T, (width, height))
 
-    right_eye_relative = np.array(eyes)[1, 0:2] + translation - eyes_center
-    angle = math.atan(right_eye_relative[1] / right_eye_relative[0])
+    # Rotate
+    eye_right_relative = eye_right - eyes_center
+    angle = math.atan(eye_right_relative[1] / eye_right_relative[0])
     image = imutils.rotate(image, angle)
 
-    # distance = math.sqrt(right_eye_relative[0] ** 2 + right_eye_relative[1] ** 2)
-    # scale = avg_distance / distance
-    # image = cv2.resize(image, (int(width * scale), int(height * scale)))
+    # Resize
+    scale = eyes_dist_avg / eyes_dist[input_file]
+    image = cv2.resize(image, (int(width * scale), int(height * scale)))
 
-    cv2.imwrite(f"{output_dir}/{os.path.basename(input_file)}", image)
+    # Write
+    cv2.imwrite(f"{output_temp_dir}/{os.path.basename(input_file)}", image)
 
-    new_height, new_width = image.shape[:2]
-    max_width, max_height = max(max_width, new_width), max(max_height, new_height)
+    # Store largest image seen
+    height_new, width_new = image.shape[:2]
+    width_min, height_min = min(width_min, width_new), min(height_min, height_new)
 
-max_width = max_width if max_width % 2 == 0 else max_width + 1
-max_height = max_height if max_height % 2 == 0 else max_height + 1
+# Make dimensions even
+width_min = width_min if width_min % 2 == 0 else width_min - 1
+height_min = height_min if height_min % 2 == 0 else height_min - 1
 
-# Make images same size
-# print("Reshaping")
-# for input_file in glob.glob(f"{output_dir}/*.jpg"):
-#     image = cv2.imread(input_file)
-#     height, width = image.shape[:2]
-#
-#     vert_border = max_height - height
-#     top_border = int(vert_border)
-#     bottom_border = int(vert_border) if vert_border == int(vert_border) else int(vert_border + 1)
-#
-#     hor_border = max_width - width
-#     left_border = int(hor_border)
-#     right_border = int(hor_border) if hor_border == int(hor_border) else int(hor_border + 1)
-#
-#     image = cv2.copyMakeBorder(image, top_border, bottom_border, left_border, right_border, cv2.BORDER_CONSTANT)
-#     cv2.imwrite(f"{output_dir}/{os.path.basename(input_file)}", image)
+# Reshape
+for input_file in tqdm(glob.glob(f"{output_temp_dir}/*.jpg"), desc="Reshaping"):
+    image = cv2.imread(input_file)
+    height, width = image.shape[:2]
+
+    width_excess = width - width_min
+    width_start = int(width_excess / 2)
+    width_end = width_start + width_min
+
+    height_excess = height - height_min
+    height_start = int(height_excess / 2)
+    height_end = height_start + height_min
+
+    image = image[height_start:height_end, width_start:width_end]
+    cv2.imwrite(f"{output_final_dir}/{os.path.basename(input_file)}", image)
